44c5f683e962404f859714dd2f154cd70c7ab588
Angela <angelagn50@gmail.com>
2023-02-22 13:35:02 +0100

    Add MeetStrangers. java file

---
+++ Excersice1/MeetStrangers.java
@@ -0,0 +1,18 @@
+package Excersice1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MeetStrangers {
+    public static void main(String[] args) throws IOException {
+        //Write a program, asks for a number - amount of strangers to meet.
+        //Then reads stranger names line by line and prints line by line "Hello, ...".
+        Scanner sc = new Scanner(System.in);
+        Integer input = sc.nextInt(); 
+        
+        //System.out.println("Hello, Agent" + input);
+        
+    }
+
+    
+}
